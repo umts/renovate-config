344528bc11ce3cdc377bc6fb6d6f1afceeddde40
benmelz <ben.j.melz@gmail.com>
2026-08-05 13:39:47 -0400

    Onboard sinatra and vite repos (#31)

--- config.js
+++ config.js
@@ -1,13 +1,18 @@
 module.exports = {
   onboardingConfig: {
     extends: [
-      'config:recommended',
       'github>umts/renovate-config:base',
     ]
   },
   onboardingRebaseCheckbox: true,
   platform: 'github',
   repositories: [
+    'umts/departure-board',
+    'umts/dev-training-web',
+    'umts/fleetfocus-api',
+    'umts/gtfs_cache',
+    'umts/gtfs-react-hooks',
+    'umts/public-message-board',
     'umts/round-three',
     'umts/umaps-rails',
   ],
